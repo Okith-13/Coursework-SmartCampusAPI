e7ba06adb5acc1b72203e3b66ae1ed5e0b98fd54
Okith Hettihewa <okithhettihewa@gmail.com>
2026-04-21 10:20:57 +0530

    First Commit

---
+++ NetBeansProjects/SmartCampusAPI/src/main/java/com/mycompany/smartcampusapi/config/ApplicationConfig.java
@@ -0,0 +1,14 @@
+package com.mycompany.smartcampusapi.config;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author okith
+ */
+public class ApplicationConfig {
+    
+}
